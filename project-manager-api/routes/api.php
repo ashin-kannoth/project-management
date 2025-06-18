6d64381f3d1c82b9bc963037a0bc26dc91469a1d
ashin-kannoth <ashinkannoth@gmail.com>
2025-06-19 01:44:33 +0530

    Update api.php

--- project-manager-api/routes/api.php
+++ project-manager-api/routes/api.php
@@ -1,1 +1,12 @@
-// API routes with auth and project CRUD
+use App\Http\Controllers\API\ProjectController;
+use App\Http\Controllers\API\AuthController;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/projects', [ProjectController::class, 'index']);
+    Route::post('/projects', [ProjectController::class, 'store']);
+    Route::put('/projects/{project}', [ProjectController::class, 'update']);
+    Route::delete('/projects/{project}', [ProjectController::class, 'destroy']);
+});
